b9f40aeb14519336f62d546f45d5bc0ace26b21c
Mehrzad <mehrzad20061384@gmail.com>
2025-03-22 14:57:33 +0330

    initial project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+
+  <div class="hi">
+    <h1>Hello VueJs</h1>
+  </div>
+
+</template>
+
+
+<style scoped>
+.hi {
+  background-color: #000;
+  color: #fff;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px;
+  text-align: center;
+}
+</style>
