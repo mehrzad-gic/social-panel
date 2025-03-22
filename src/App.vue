e82f34505a9088ba5feb29c092ebd2cff780140c
Mehrzad <mehrzad20061384@gmail.com>
2025-03-22 22:23:35 +0330

    continue of Vue Journey

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,89 @@
 <template>
+
+  <Header />
+
   <div class="container mt-5 mb-5">
-    <h1 class="green">{{ message }}</h1>
 
-    <!-- Form for creating a new post -->
-    <form @submit.prevent="createPost" class="mb-5 mt-5">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name:</label>
-        <input type="text" class="form-control" id="name" v-model="newPost.name" placeholder="Enter name" required>
-      </div>
-      <div class="mb-3">
-        <label for="status" class="form-label">Status:</label>
-        <select v-model="newPost.status" class="form-select" name="status" id="status" required>
-          <option selected value="">Choose</option>
-          <option value="1">On</option>
-          <option value="0">Off</option>
-        </select>
-      </div>
-      <div class="mb-3">
-        <label for="description" class="form-label">Description:</label>
-        <textarea class="form-control" id="description" v-model="newPost.description" rows="5"
-          placeholder="Enter description" required></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Create</button>
-    </form>
+    <Hello message-hello="Hello VueJs 💚" />
 
-    <!-- Button to toggle table visibility -->
-    <span class="btn btn-info mt-4 mb-4 h3" @click="toggleTableVisibility">{{ isShow ? 'Hide 🤪' : 'Show 🫠' }}</span>
+    <div class="row">
 
-    <!-- Table to display posts -->
-    <table class="table" v-if="isShow">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Status</th>
-          <th scope="col">Toggle</th>
-          <th scope="col">Edit</th>
-          <th scope="col">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="post in posts" :key="post.id">
-          <th scope="row">{{ post.id }}</th>
-          <td>{{ post.name }}</td>
-          <td>{{ post.description }}</td>
-          <td>{{ post.status == 1 ? '🔛' : '📴' }}</td>
-          <td><button @click="togglePostStatus(post)" class="btn btn-sm btn-outline-secondary">👽</button></td>
-          <td><button @click="editPost(post)" class="btn btn-sm btn-outline-warning">🖋️</button></td>
-          <td><button @click="deletePost(post)" class="btn btn-sm btn-outline-danger">❌</button></td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="col-8">
+
+        <!-- Button to toggle table visibility -->
+        <span class="btn btn-info mt-4 mb-4 h3" @click="toggleTableVisibility">{{ isShow ? 'Hide 🤪' : 'Show 🫠'
+          }}</span>
+
+        <!-- Table to display posts -->
+        <table class="table" v-if="isShow">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Status</th>
+              <th scope="col">Toggle</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="post in posts" :key="post.id">
+              <th scope="row">{{ post.id }}</th>
+              <td>{{ post.name }}</td>
+              <td>{{ post.description }}</td>
+              <td>{{ post.status == 1 ? '🔛' : '📴' }}</td>
+              <td><button @click="togglePostStatus(post)" class="btn btn-sm btn-outline-secondary">👽</button></td>
+              <td><button @click="editPost(post)" class="btn btn-sm btn-outline-warning">🖋️</button></td>
+              <td><button @click="deletePost(post)" class="btn btn-sm btn-outline-danger">❌</button></td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+      <div class="col-4">
+
+        <!-- Form for creating a new post -->
+        <form @submit.prevent="createPost" class="mb-5 mt-5">
+          <div class="mb-3">
+            <label for="name" class="form-label">Name:</label>
+            <input type="text" class="form-control" id="name" v-model="newPost.name" placeholder="Enter name" required>
+          </div>
+          <div class="mb-3">
+            <label for="status" class="form-label">Status:</label>
+            <select v-model="newPost.status" class="form-select" name="status" id="status" required>
+              <option selected value="">Choose</option>
+              <option value="1">On</option>
+              <option value="0">Off</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="description" class="form-label">Description:</label>
+            <textarea class="form-control" id="description" v-model="newPost.description" rows="5"
+              placeholder="Enter description" required></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
+        </form>
+
+      </div>
+
+    </div>
+
   </div>
+
 </template>
 
-<style scoped>
-.green {
-  color: rgb(38, 170, 38);
-}
-
-.table th, .table td {
-  vertical-align: middle;
-}
-</style>
 
 <script>
+import Header from './components/Header.vue';
+import Hello from './components/Hello.vue';
+
 export default {
+  components: {
+    Hello,
+    Header
+  },
   data() {
     return {
       isShow: true,
---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="green">{{messageHello}}</h1>
+</template>
+
+<script>
+export default {
+
+    // props:['message']
+    props:{
+        messageHello:{
+            require:true,
+            type:String
+        }
+    }
+}
+</script>
+
+<style scoped>
+.green {
+  color: rgb(38, 170, 38);
+}
+</style>
